f79bbc7f6169c6bc62574a3c846b3e789b829ca4
Alfa and Friends <dev@alfaandfriends.com>
2022-05-20 12:20:52 +0800

    compile

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -184,7 +184,7 @@ export default defineComponent({
     openLightbox: Boolean
   },
   created(){
-    console.log(this.images)
+    // console.log(this.images)
   },
   setup(props) {
     const state = reactive({
